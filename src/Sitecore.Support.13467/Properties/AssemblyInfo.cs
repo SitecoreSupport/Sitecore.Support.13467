2dcf36d9832a1ce7af437196c94ac94e3de82db6
DK\EVS <evs@sitecore.com>
2018-07-16 13:16:25 +0300

    initial commit with fix. a few classes has been overridden to rework the GetAttributeTokenValue method

---
+++ src/Sitecore.Support.13467/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13467")]
+[assembly: AssemblyProduct("Sitecore.Support.13467")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.13467/SupportRenderRenderingVariantFieldProcessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+
+namespace Sitecore.Support.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField
+{
+  public class RenderText : Sitecore.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField.RenderText
+  {
+    protected override string GetAttributeTokenValue(string fieldName, Item item)
+    {
+      Field field = item.Fields[fieldName];
+      if (field != null)
+      {
+        CustomField field2 = FieldTypeManager.GetField(field);
+        if (field2 is ValueLookupField)
+        {
+          return ((ValueLookupField)item.Fields[fieldName]).Value;
+        }
+        else
+        {
+          return base.GetAttributeTokenValue(fieldName, item);
+        }
+      }
+      else
+        return String.Empty;
+    }
+  }
+
+  public class RenderVariantField: Sitecore.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField.RenderVariantField
+  {
+    protected override string GetAttributeTokenValue(string fieldName, Item item)
+    {
+      Field field = item.Fields[fieldName];
+      if (field != null)
+      {
+        CustomField field2 = FieldTypeManager.GetField(field);
+        if (field2 is ValueLookupField)
+        {
+          return ((ValueLookupField)item.Fields[fieldName]).Value;
+        }
+        else
+        {
+          return base.GetAttributeTokenValue(fieldName, item);
+        }
+      }
+      else
+        return String.Empty;
+    }
+  }
+}
