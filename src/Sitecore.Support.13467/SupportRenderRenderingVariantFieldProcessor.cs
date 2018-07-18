b14ecade639f00ff48e52c9da70b730d3e3e37bf
DK\EVS <evs@sitecore.com>
2018-07-18 09:59:18 +0300

    RenderSection class patching has been added

--- src/Sitecore.Support.13467/SupportRenderRenderingVariantFieldProcessor.cs
+++ src/Sitecore.Support.13467/SupportRenderRenderingVariantFieldProcessor.cs
@@ -50,4 +50,27 @@ namespace Sitecore.Support.XA.Foundation.RenderingVariants.Pipelines.RenderVaria
         return String.Empty;
     }
   }
+
+
+  public class RenderSection : Sitecore.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField.RenderSection
+  {
+    protected override string GetAttributeTokenValue(string fieldName, Item item)
+    {
+      Field field = item.Fields[fieldName];
+      if (field != null)
+      {
+        CustomField field2 = FieldTypeManager.GetField(field);
+        if (field2 is ValueLookupField)
+        {
+          return ((ValueLookupField)item.Fields[fieldName]).Value;
+        }
+        else
+        {
+          return base.GetAttributeTokenValue(fieldName, item);
+        }
+      }
+      else
+        return String.Empty;
+    }
+  }
 }
